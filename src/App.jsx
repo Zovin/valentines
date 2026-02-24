b5e6e17275d569abcde21affeba131ffffce3496
Zovin Anggleas <anggleaszovin@gmail.com>
2026-02-25 03:54:28 +1100

    set up github pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 
 import './App.css'
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { HashRouter, Routes, Route } from "react-router-dom"
 import Home from './pages/Home'
 import Timeline from './pages/Timeline'
 import Memories from './pages/Memories'
@@ -9,14 +9,14 @@ import Scratch from './pages/Scratch'
 function App() {
 
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/timeline" element={<Timeline />} />
         <Route path="/memories" element={<Memories />} />
         <Route path="/scratch" element={<Scratch />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
